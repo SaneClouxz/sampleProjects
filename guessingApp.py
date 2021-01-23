8068c8dbef5ff19eac52fb0508cb972e0b0e1691
SaneClouxz <miracleclouxz@gmail.com>
2021-01-23 01:03:54 -0800

    new commits, made changes to the tempConv.py to prevent some use input, plus a new py file

---
+++ guessingApp.py
@@ -0,0 +1,30 @@
+import random
+
+# creating the random guessed number between 1 and 10
+secret_number = random.randint(1, 10)
+
+# number of user secret_number which is 3
+number_of_guess = 3
+
+# secret_number used which is increased after every count
+guess_count = 0
+
+# creating the game
+valid_input = True
+
+while guess_count < number_of_guess:
+    user_guess = int(input("> Guess the secret number: "))
+    if user_guess < secret_number:
+        print("Your guess is less than the number, try again!")
+        guess_count += 1
+
+    elif user_guess > secret_number:
+        print("Your guess is greater than the number, try again!")
+        guess_count += 1
+
+    elif user_guess == secret_number:
+        print("You guessed right, you won!!")
+        break
+
+else:
+    print(f"You lost, the secret number is {secret_number} 😜")
--- tempConv.py
+++ tempConv.py
@@ -1,45 +1,59 @@
-import sys
-
 # taking user temp value
-try:
-    temp = float(input("Enter temperature value: "))
-except ValueError:
-    print("Enter numeric unit, exiting program, try again!")
-    sys.exit()
+out = True
+while out:
+    try:
+        temperature = float(input("Enter temperature value: "))
+        out = False
+    except ValueError:
+        print("Enter numeric unit, try again!")
 
 # taking user input unit
-temp_unit = input("Enter input unit, C for Celsius, K for Kelvin  or F for Fahreheit: ")
+# the while loop is to prevent the user from entering a different option from the ones provided
+
+valid_in = True
+while valid_in:
+    temp_unit = input("Enter input unit, C for Celsius, K for Kelvin  or F for Fahrenheit: ")
+    if temp_unit.lower() == "c":
+        valid_in = False
+    elif temp_unit.lower() == "f":
+        valid_in = False
+    elif temp_unit.lower() == "k":
+        valid_in = False
+
+    else:
+        print("Incorrect input, enter a valid option!")
+        valid_in = True
 
 # creating th logics and formula
 if temp_unit.lower() == "c":
     # prompt user for unit to convert to
     conversion_unit = input("Enter output unit, K for Kelvin or F for Fahrenheit: ")
     if conversion_unit.lower() == "k":
-        converted_temp = temp + 273
+        converted_temp = temperature + 273
         print(f"Temperature is {converted_temp} degrees in kelvin from Celsius!")
 
     elif conversion_unit.lower() == "f":
-        converted_temp = 1.8 * temp + 32
+        converted_temp = 1.8 * temperature + 32
         print(f"Temperature is {converted_temp} degrees in fahrenheit from Celsius!")
 
 elif temp_unit.lower() == "k":
     # prompt user for unit to convert to
     conversion_unit = input("Enter output unit, C for Celsius or F for Fahrenheit: ")
     if conversion_unit.lower() == "c":
-        converted_temp = temp - 273
+        converted_temp = temperature - 273
         print(f"Temperature is {converted_temp} degrees in Celsius from Kelvin!")
 
     elif conversion_unit.lower() == "f":
-        converted_temp = 1.8 * (temp - 273) + 32
+        converted_temp = 1.8 * (temperature - 273) + 32
         print(f"Temperature is {converted_temp} degrees in Fahrenheit from Kelvin!")
 
 elif temp_unit.lower() == "f":
     # prompt user for unit to convert to
     conversion_unit = input("Enter output unit, K for Kelvin or C for Celsius: ")
     if conversion_unit.lower() == "c":
-        converted_temp = 0.6 * (temp - 32)
+        converted_temp = 0.6 * (temperature - 32)
         print(f"Temperature is {converted_temp} degrees in Celsius from Fahrenheit!")
 
     elif conversion_unit.lower() == "k":
-        converted_temp = 0.6 * (temp - 32) + 273
+        converted_temp = 0.6 * (temperature - 32) + 273
         print(f"Temperature is {converted_temp} degrees in Kelvin from Fahrenheit!")
